b4a7ea347cf6b1830aaca3f1a710999d66c2724f
maggieedavisss <151679687+maggieedavisss@users.noreply.github.com>
2024-10-17 18:19:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,23 @@ def favicon():
 
 @app.route('/sql-chatbot')
 def sql_chatbot():
-    return "This will be the SQL Chatbot page"  # Placeholder for the SQL Chatbot page
+    return redirect("https://python-flask-sqlbot-app.azurewebsites.net/")
 
 @app.route('/pdf-narrator')
 def pdf_narrator():
-    return redirect("https://gen-ai-web-app.azurewebsites.net/document_insight_via_chat")
+    return redirect("https://gen-ai-web-app.azurewebsites.net/document_insight_via_chat")  
+
+@app.route('/powerbi-demo')
+def powerbi_demo():
+    return redirect("https://app.powerbi.com/groups/me/apps/1f7cd510-8c93-4e5f-8cef-312f3bbf0d01/reports/363ee684-634c-43da-b040-8539933cbf13/42ba0471426783ecc124?ctid=978794b6-9c6b-4307-ada4-42e472e0e336&experience=power-bi")
+
+@app.route('/azure-etf-ratings-chatbot')
+def azure_etf_ratings_chatbot():
+    return redirect("https://morningstar-ratings.azurewebsites.net/")
+
+@app.route('/azure-etf-returns-chatbot')
+def azure_etf_returns_chatbot():
+    return redirect("https://morningstar-returns.azurewebsites.net/")  
 
 if __name__ == '__main__':
     app.run(debug=True)
